6bdb7658d25bf5f4478227b3f00a5d0f2bd3d250
bank8426 <bank_55555@hotmail.com>
2019-06-07 15:49:14 +0700

    question event handling

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,14 +6,18 @@ let quizData = require('./quiz_data.json')
 class Quiz extends Component{
     constructor(props){
         super(props)
-        this.state = {quiz_position: 2}
+        this.state = {quiz_position: 1}
+    }
+
+    showNextQuestion(){
+        this.setState({quiz_position: this.state.quiz_position+1})
     }
 
     render(){
         const isQuizEnd = this.state.quiz_position-1 === quizData.quiz_questions.length
         return(
             <div>
-                { isQuizEnd ? <QuizEnd/> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} />}
+                { isQuizEnd ? <QuizEnd/> : <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} />}
             </div>
         )
         
